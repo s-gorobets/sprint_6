9fb1cfbf06f59e1e3f699f14ee706e541a68211c
Sergei Gorobets <sergei.gorobets@vk.team>
2025-06-06 19:35:49 +0300

    scooter1

--- pages/order_page.py
+++ pages/order_page.py
@@ -20,18 +20,22 @@ class OrderPage(Base_page):
     @allure.step("Заполняем поле 'Адрес' на странице заказа")
     def send_address_in_order_page(self, address):
         self.add_text_element(ORDER_LOCATORS.ADDRESS_FIELD, address)
+
     @allure.step("Выбираем станцию метро")
     def select_metro_station_in_order_page(self, metro):
         field_metro = self.find_element(ORDER_LOCATORS.METRO_DROPDOWN)
         field_metro.click()
         field_metro.send_keys(metro)
+
         self.find_element(ORDER_LOCATORS.select_metro_station_option(metro)).click()
     @allure.step("Заполняем поле 'Телефон'")
     def send_phone_in_order_page(self, phone):
         self.add_text_element(ORDER_LOCATORS.PHONE_NUMBER_FIELD, keys=phone)
+
     @allure.step("Переходим на вторую страницу заказа")
     def click_next_in_order_page(self):
         self.click_element(ORDER_LOCATORS.ORDER_BUTTON)
+
     @allure.step("Заполняем дату аренды")
     def select_date_start(self, rent_date):
         self.find_element(ORDER_LOCATORS.RENT_DATE_INPUT).send_keys(rent_date)
@@ -39,13 +43,13 @@ class OrderPage(Base_page):
 
     @allure.step("Выбираем срок аренды")
     def select_rent_period_in_order(self):
-        print("Метод select_rent_period_in_order вызван!")
         self.click_element(ORDER_LOCATORS.RENT_TERMS_DROPDOWN)
         self.click_element(ORDER_LOCATORS.RENT_TERMS_4_DAYS)
 
     @allure.step("Выбираем чёрный цвет самоката")
     def select_black_color_in_order_page(self):
         self.click_element(ORDER_LOCATORS.SCOOTER_COLOR_BLACK_CHECKBOX)
+        
     @allure.step("Оставляем комментарий для курьера")
     def add_comment_for_courier(self, comment):  # Оставляем комментарий для курьера
         self.add_text_element(ORDER_LOCATORS.COMMENTS_FIELD, keys=comment)
